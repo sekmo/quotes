33aa0e441c699e59174b2a1d11206e92c0335d70
Francesco Mari <francescojjmari@gmail.com>
2017-11-18 18:15:36 +0100

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Quote.create! (
   [
     {
-      text: "The secret of change is to focus all of your energy, not on fighting the old, but on \
-      building the new.",
+      text: "The secret of change is to focus all of your energy, not on fighting the old, but " \
+        "on building the new.",
       author: "Socrates"
     },
     {
@@ -10,8 +10,8 @@ Quote.create! (
       author: "Preston Ni"
     },
     {
-      text: "The greatest challenge to any thinker is stating the problem in a way that will \
-      allow a solution.",
+      text: "The greatest challenge to any thinker is stating the problem in a way that will " \
+        "allow a solution.",
       author: "Bertrand Russell"
     },
     {
@@ -35,8 +35,8 @@ Quote.create! (
       author: "Sheryl Sandberg"
     },
     {
-      text: "You can't be that kid standing at the top of the water slide, overthinking it. \
-      You have to go down the chute.",
+      text: "You can't be that kid standing at the top of the water slide, overthinking it. " \
+        "You have to go down the chute.",
       author: "Tina Fey"
     },
     {
@@ -48,12 +48,12 @@ Quote.create! (
       author: "Tenet #2 of the UNIX philosophy"
     },
     {
-      text: "Often people, especially computer engineers, focus on the machines. They think, \
-      ‘By doing this, the machine will run faster. By doing this, the machine will run more \
-      effectively. By doing this, the machine will something something something.’ They are \
-      focusing on machines. But in fact we need to focus on humans, on how humans care about \
-      doing programming or operating the application of the machines. We are the masters. They \
-      are the slaves.",
+      text: "Often people, especially computer engineers, focus on the machines. They think, " \
+        "‘By doing this, the machine will run faster. By doing this, the machine will run more " \
+        "effectively. By doing this, the machine will something something something.’ They are " \
+        "focusing on machines. But in fact we need to focus on humans, on how humans care about " \
+        "doing programming or operating the application of the machines. We are the masters. " \
+        "They are the slaves.",
       author: "Yukihiro Matsumoto"
     }
   ]
