0fe1b3e380465da28f893743dfa2f7c88a6f4d58
Francesco Mari <francescojjmari@gmail.com>
2017-11-18 18:20:10 +0100

    Add QuotesDisplayer component

--- app/javascript/quotes/components/App.jsx
+++ app/javascript/quotes/components/App.jsx
@@ -3,5 +3,17 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import QuotesDisplayer from './QuotesDisplayer';
+
+const App = (props) => (
+  <Router>
+    <div>
+      <Route
+        path='/'
+        component={QuotesDisplayer}
+      />
+    </div>
+  </Router>
+);
 
 export default App;
