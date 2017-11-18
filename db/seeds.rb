29c8827ea1da9b9febc182b1be47650f06123333
Francesco Mari <francescojjmari@gmail.com>
2017-11-18 15:44:32 +0100

    Add Quote model with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Quote.create! (
+  [
+    {
+      text: "The secret of change is to focus all of your energy, not on fighting the old, but on \
+      building the new.",
+      author: "Socrates"
+    },
+    {
+      text: "Don’t confuse the urgent with the important.",
+      author: "Preston Ni"
+    },
+    {
+      text: "The greatest challenge to any thinker is stating the problem in a way that will \
+      allow a solution.",
+      author: "Bertrand Russell"
+    },
+    {
+      text: "Don't let perfection become procrastination. Do it now.",
+      author: "Danielle LaPorte"
+    },
+    {
+      text: "The greatest of all weaknesses is the fear of appearing weak",
+      author: "Jacques-Bénigne Bossuet"
+    },
+    {
+      text: "Life has no limitations, except the ones you make.",
+      author: "Les Brown"
+    },
+    {
+      text: "A gem cannot be polished without friction, nor a man perfected without trials.",
+      author: "Seneca"
+    },
+    {
+      text: "Done is better than perfect.",
+      author: "Sheryl Sandberg"
+    },
+    {
+      text: "You can't be that kid standing at the top of the water slide, overthinking it. \
+      You have to go down the chute.",
+      author: "Tina Fey"
+    },
+    {
+      text: "Great things are done by a series of small things brought together.",
+      author: "Vincent Van Gogh"
+    },
+    {
+      text: "Make each program do one thing well.",
+      author: "Tenet #2 of the UNIX philosophy"
+    },
+    {
+      text: "Often people, especially computer engineers, focus on the machines. They think, \
+      ‘By doing this, the machine will run faster. By doing this, the machine will run more \
+      effectively. By doing this, the machine will something something something.’ They are \
+      focusing on machines. But in fact we need to focus on humans, on how humans care about \
+      doing programming or operating the application of the machines. We are the masters. They \
+      are the slaves.",
+      author: "Yukihiro Matsumoto"
+    }
+  ]
+)
