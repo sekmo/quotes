387925701f9fd8e6293ed90beaa14da522906af6
Francesco Mari <francescojjmari@gmail.com>
2017-11-20 17:25:30 +0100

    Remove index.js from app/javascript/quotes

--- app/javascript/packs/quotes.js
+++ app/javascript/packs/quotes.js
@@ -1,1 +1,7 @@
-import 'quotes'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../quotes/components/App';
+
+const quotesDiv = document.querySelector('.quotes');
+const firstQuoteId = quotesDiv.dataset.firstQuoteId;
+ReactDOM.render(<App firstQuoteId={firstQuoteId} />, quotesDiv);
--- app/javascript/quotes/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-const quotesDiv = document.querySelector('.quotes');
-const firstQuoteId = quotesDiv.dataset.firstQuoteId;
-ReactDOM.render(<App firstQuoteId={firstQuoteId} />, quotesDiv);
